c9489bc73586bb9058c5969177d4f6c0fb9e5e21
João Vitor Camelo <133178723+joaovitorcamelo@users.noreply.github.com>
2023-07-12 10:23:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-poetry self update
-poetry lock --no-update
-poetry install
+pip install --upgrade pip
+pip install poetry
+/opt/render/project/src/.venv/bin/poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
