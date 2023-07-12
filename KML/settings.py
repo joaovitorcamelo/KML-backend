b3c2a3014b8f5379c618eb5fcd46d643be1d2549
João Vitor Camelo <133178723+joaovitorcamelo@users.noreply.github.com>
2023-07-12 13:48:03 -0300

    AA

--- KML/settings.py
+++ KML/settings.py
@@ -13,21 +13,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-jpf^m-1h)nzgablh^ceos9zlg*l3t-c%h*5n+^ure7-s$%jx!x')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['https://kml.onrender.com', 'http://0.0.0.0:10000']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -81,11 +83,7 @@ WSGI_APPLICATION = 'KML.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://wuhzbffv:hyftyAQW2UjXEHigfp-qZfeudAe_1LqK@silly.db.elephantsql.com/wuhzbffv',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
