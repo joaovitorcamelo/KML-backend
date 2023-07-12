37606c130599018fe5017b4eac11caf9e5586610
João Vitor Camelo <133178723+joaovitorcamelo@users.noreply.github.com>
2023-07-12 02:21:46 -0300

    database

--- KML/settings.py
+++ KML/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,11 @@ WSGI_APPLICATION = 'KML.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgres://wuhzbffv:hyftyAQW2UjXEHigfp-qZfeudAe_1LqK@silly.db.elephantsql.com/wuhzbffv',
+        conn_max_age=600
+    )
 }
 
 
