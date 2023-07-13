9587c66d352ffc1e24b9a20e85925cf7911b5cf7
João Vitor Camelo <133178723+joaovitorcamelo@users.noreply.github.com>
2023-07-13 18:45:32 -0300

    Update settings.py

--- KML/settings.py
+++ KML/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000/']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'KML.urls'
 
