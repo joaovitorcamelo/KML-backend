1090f33813b8d775bee248617e226493f6f4cf83
João Vitor Camelo <133178723+joaovitorcamelo@users.noreply.github.com>
2023-07-13 11:24:13 -0300

    Auth fix

--- google_connection/urls.py
+++ google_connection/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create/", views.create, name="createSheets"),
-    path("send/all/", views.send, name="send"),
-    path("delete/all/", views.delete, name="delete"),
-    path("login/", views.login, name="login"),
-    path("send/<str:dre>/", views.send_one, name="sendSome"),
-    path("delete/<str:dre>/", views.delete_one, name="deleteSome"),
+    path("create", views.create, name="createSheets"),
+    path("send/all", views.send, name="send"),
+    path("delete/all", views.delete, name="delete"),
+    path("auth", views.oauth_redirect, name="auth"),
+    path("callback", views.oauth_callback, name="callback"),
+    path("send/<str:dre>", views.send_one, name="sendSome"),
+    path("delete/<str:dre>", views.delete_one, name="deleteSome"),
 ]
--- google_connection/views.py
+++ google_connection/views.py
@@ -1,24 +1,28 @@
 import ast
 import time
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import User
 import gspread
-import json
+from gspread import oauth
 import os
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
+SCOPES = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
 
-# Create your views here.
+CREDENTIALS = ast.literal_eval(os.environ.get('CREDENTIALS'))
 
 def delete_one(request, dre):
     try:
-        credentials_STR = os.environ.get('CREDENTIALS')
-        credentials = ast.literal_eval(credentials_STR)
 
         email = request.session['email']
         user = User.objects.filter(email=email)
         authorized_user = ast.literal_eval(user[0].user_key)
 
-        gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
+        gc, authorized_user = gspread.oauth_from_dict(CREDENTIALS, authorized_user)
 
         sh = gc.open('Medicina 259')
         worksheet = sh.sheet1
@@ -39,14 +43,12 @@ def delete_one(request, dre):
 
 def send_one(request, dre):
     try:
-        credentials_STR = os.environ.get('CREDENTIALS')
-        credentials = ast.literal_eval(credentials_STR)
 
         email = request.session['email']
         user = User.objects.filter(email=email)
         authorized_user = ast.literal_eval(user[0].user_key)
 
-        gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
+        gc, authorized_user = gspread.oauth_from_dict(CREDENTIALS, authorized_user)
 
         sh = gc.open('Medicina 259')
         worksheet = sh.sheet1
@@ -66,14 +68,12 @@ def send_one(request, dre):
 
 def create(request):
     try:
-        credentials_STR = os.environ.get('CREDENTIALS')
-        credentials = ast.literal_eval(credentials_STR)
 
         email = request.session['email']
         user = User.objects.filter(email=email)
         authorized_user = ast.literal_eval(user[0].user_key)
 
-        gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
+        gc, authorized_user = gspread.oauth_from_dict(CREDENTIALS, authorized_user)
 
         sh = gc.open("Medicina 259")
         worksheet = sh.sheet1
@@ -115,14 +115,12 @@ def create(request):
 
 
 def send(request):
-    credentials_STR = os.environ.get('CREDENTIALS')
-    credentials = ast.literal_eval(credentials_STR)
 
     email = request.session['email']
     user = User.objects.filter(email=email)
     authorized_user = ast.literal_eval(user[0].user_key)
 
-    gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
+    gc, authorized_user = gspread.oauth_from_dict(CREDENTIALS, authorized_user)
 
     sh = gc.open('Medicina 259')
     worksheet = sh.sheet1
@@ -147,15 +145,13 @@ def send(request):
 
 
 def delete(request):
-    credentials_STR = os.environ.get('CREDENTIALS')
-    credentials = ast.literal_eval(credentials_STR)
 
     email = request.session['email']
     user = User.objects.filter(email=email)
+
     authorized_user = ast.literal_eval(user[0].user_key)
 
-    gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
-
+    gc, authorized_user = gspread.oauth_from_dict(CREDENTIALS, authorized_user)
 
     sh = gc.open('Medicina 259')
     worksheet = sh.sheet1
@@ -174,33 +170,60 @@ def delete(request):
     return HttpResponse("Remoção realizada com sucesso!")
 
 
-def login(request):
+def oauth_redirect(request):
+
+    email = request.GET.get('email')
+    request.session['email'] = email
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(
+        CREDENTIALS,
+        scopes=SCOPES
+    )
+
+    flow.redirect_uri = '/callback'
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true'
+    )
+
+    request.session['state'] = state
+
+    return HttpResponseRedirect(authorization_url)
+
+
+def oauth_callback(request):
+    state = request.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(
+        CREDENTIALS,
+        scopes=SCOPES,
+        state=state
+    )
+
+    flow.redirect_uri = 'callback'
+
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    user_key = ast.literal_eval(flow.credentials)
+
+    request.session['user_key'] = user_key
+
     try:
-        email = request.GET.get('email')
+        email = request.session['email']
         user = User.objects.filter(email=email)
 
         if user:
-            credentials_STR = os.environ.get('CREDENTIALS')
-            credentials = ast.literal_eval(credentials_STR)
-
-            authorized_user = ast.literal_eval(user[0].user_key)
-
-            gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
-
-            user[0].user_key = authorized_user
+            user[0].user_key = user_key
             user[0].save()
 
         else:
-            credentials_STR = os.environ.get('CREDENTIALS')
-            credentials = ast.literal_eval(credentials_STR)
-            gc, authorized_user = gspread.oauth_from_dict(credentials)
-
-            new_user = User(email=email, user_key=authorized_user)
+            new_user = User(email=email, user_key=user_key)
             new_user.save()
-
-        request.session['email'] = email
 
         return HttpResponse("Login Efetuado.")
 
     except:
         raise Http404("Ocorreu um erro.")
+
